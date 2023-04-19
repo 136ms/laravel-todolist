6287e142f974bfecbbe9b07e3a564231677d78b8
136ms <85730995+136ms@users.noreply.github.com>
2023-04-19 21:26:03 +0200

    [master] modified route

--- laravel-todolist/routes/web.php
+++ laravel-todolist/routes/web.php
@@ -17,13 +17,20 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::user())
+    {
+        return view('tasks.index');
+    }
+    else{
+        return view('auth.login');
+    }
 });
 
 Route::get('/home', [App\Http\Controllers\TaskController::class, 'index'])->name('home');
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::get('/tasks/destroy/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::get('/tasks/destroy', [TaskController::class, 'destroyAll'])->name('tasks.destroy.all');
 Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
